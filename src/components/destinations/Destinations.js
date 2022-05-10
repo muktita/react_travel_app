47310c10019b57132cbecbdb8658537723c84eff
muktita <muktitkim@csu.fullerton.edu>
2022-05-10 16:48:02 -0700

    added images to destination

--- src/components/destinations/Destinations.js
+++ src/components/destinations/Destinations.js
@@ -2,11 +2,27 @@ import React from "react";
 
 import "./DestinationStyles.css";
 
+import BoraBora from "../../assests/borabora.jpg"
+import BoraBora2 from "../../assests/borabora2.jpg"
+import Maldives from "../../assests/maldives.jpg"
+import Maldvies2 from "../../assests/maldives2.jpg"
+import KeyWest from "../../assests/keywest.jpg"
+
 
 function Destinations() {
     return (
-        <div>
-            Hi!
+        <div className="destinations">
+            <div className="container">
+                <h1> All Inclusive Resorts</h1>
+                <p> On the Carribean's Best Beaches</p>
+                <div className="img-container">
+                    <img src={BoraBora} alt="/"/>
+                    <img src={BoraBora2} alt="/"/>
+                    <img src={Maldives} alt="/"/>
+                    <img src={Maldvies2} alt="/"/>
+                    <img src={KeyWest} alt="/"/>
+                </div>
+            </div>
         </div>
     )
 }
